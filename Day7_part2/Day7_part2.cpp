94eaecddc71f176e7dc8abeb008d1c630b19b0d8
jmpaull <jmpaull@umich.edu>
2024-12-07 21:39:33 -0500

    Day7_part2

--- Day7_part2/Day7_part2.cpp
+++ Day7_part2/Day7_part2.cpp
@@ -1,13 +1,14 @@
-// Day7_part1.cpp : 
+// Day7_part2.cpp : 
 
 #include <iostream>
 #include <fstream>
 #include <string>
 #include <sstream>
 #include <vector>
+#include <cmath>
 
 
-bool checkValid(unsigned long long &target, std::vector<int>::reverse_iterator start, std::vector<int>::reverse_iterator end);
+bool checkValid(unsigned long long target, std::vector<int> nums);
 
 int main()
 {
@@ -31,7 +32,7 @@ int main()
         }
 
         //Check wether the value can be achieved through the given operands
-        if (checkValid(result, nums.rend(), nums.rbegin()))
+        if (checkValid(result, nums))
         {
             sum += result;
         }
@@ -40,23 +41,29 @@ int main()
     std::cout << "total calibration result: " << sum << std::endl;
 }
 
-bool checkValid(unsigned long long &target, std::vector<int>::reverse_iterator start, std::vector<int>::reverse_iterator end)
+bool checkValid(unsigned long long target, std::vector<int> nums)
 {
-    //Base case
-    if (end == start)
+    auto end = nums.rbegin();
+    auto start = nums.rend();
+    //base case
+    if (nums.size() == 1)
     {
-        if (target == 1)
+        if (target == nums.at(0))
         {
             return true;
         }
         return false;
     }
-    
+
+    std::vector<int> next = nums;
+    next.pop_back();
+
     //multiplication
     if (target % *end == 0)
     {
         unsigned long long div = target / *end;
-        if (checkValid(div, start, std::next(end, 1)))
+        
+        if (checkValid(div, next))
         {
             return true;
         }
@@ -64,9 +71,22 @@ bool checkValid(unsigned long long &target, std::vector<int>::reverse_iterator s
 
     //addition
     unsigned long long dif = target - *end;
-    if (checkValid(dif, start, std::next(end, 1)))
+    if (checkValid(dif, next))
     {
         return true;
+    }
+
+    //concatination
+    //If last x digits are equal to the number at the end of the vector, drop them from target and check again
+    std::string disjoint = std::to_string(*end);
+    std::string targ = std::to_string(target);
+    if (targ.size() > disjoint.size() && targ.substr(targ.size() - disjoint.size()) == disjoint)
+    {
+        std::string dis = targ.substr(0, targ.size() - disjoint.size());
+        if (checkValid(std::stoull(dis), next))
+        {
+            return true;
+        }
     }
 
     return false;
