2bd286fd11c76616d04e4220f36bd6ed535e42fc
jmpaull <jmpaull@umich.edu>
2024-12-17 00:07:04 -0500

    Day13_part2

---
+++ Day13_part2/Day13_part2.cpp
@@ -0,0 +1,88 @@
+// Day13_part2.cpp : 
+
+#include <iostream>
+#include <fstream>
+#include <string>
+#include <regex>
+#include <cmath>
+
+struct Button
+{
+    int x;
+    int y;
+    int cost;
+};
+
+struct Prize
+{
+    unsigned long long x;
+    unsigned long long y;
+};
+
+unsigned long long calcMin(Button& a, Button& b, Prize& p);
+
+int main()
+{
+    const unsigned long long offset = 10000000000000;
+    std::ifstream infile("Input.txt");
+    std::string line;
+    Button a;
+    Button b;
+    Prize p;
+    a.cost = 3;
+    b.cost = 1;
+    unsigned long long total = 0;
+
+    while (std::getline(infile, line))
+    {
+        std::regex rgx("X\\+(\\d+).*Y\\+(\\d+)");
+        std::smatch match;
+        if (line.find("Button A:") != line.npos)
+        {
+            std::regex_search(line, match, rgx);
+            a.x = stoi(match[1]);
+            a.y = stoi(match[2]);
+        }
+        else if (line.find("Button B:") != line.npos)
+        {
+            std::regex_search(line, match, rgx);
+            b.x = stoi(match[1]);
+            b.y = stoi(match[2]);
+        }
+        else if (line.find("Prize:") != line.npos)
+        {
+            rgx.assign("X=(\\d+).*Y=(\\d+)");
+            std::regex_search(line, match, rgx);
+            p.x = stoi(match[1]) + offset;
+            p.y = stoi(match[2]) + offset;
+
+            total += calcMin(a, b, p);
+        }
+
+    }
+    std::cout << "The fewest tokens to will all possible prizes is: " << total << std::endl;
+}
+
+unsigned long long calcMin(Button& a, Button& b, Prize& p)
+{
+    long long det = a.x * b.y - b.x * a.y;
+
+    long long x = ((b.y * p.x) - (b.x * p.y));
+    long long y = ((a.x * p.y) - (a.y * p.x));
+
+    if (x % det == 0 && y % det == 0)
+    {
+        x /= det;
+        y /= det;
+    }
+    else
+    {
+        return 0;
+    }
+
+    if (x * a.x + y * b.x == p.x && x * a.y + y * b.y == p.y)
+    {
+        return x * a.cost + y * b.cost;
+    }
+    return 0;
+}
